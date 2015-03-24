24cb4165d84b46c7b0ed290e24c3dbbc7840f8e2
Aaron Douglas <astralbodies@gmail.com>
2015-03-24 15:40:24 -0500

    Added base TracksDemo project

---
+++ TracksDemo/TracksDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TracksDemo
+//
+//  Created by Aaron Douglas on 3/24/15.
+//  Copyright (c) 2015 Automattic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
