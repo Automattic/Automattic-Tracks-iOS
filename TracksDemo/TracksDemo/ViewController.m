5bf4c29a95958a045d0efe7dcde73b2129777daa
Aaron Douglas <astralbodies@gmail.com>
2015-04-02 12:40:25 -0500

    Added event count label to demo application

--- TracksDemo/TracksDemo/ViewController.m
+++ TracksDemo/TracksDemo/ViewController.m
@@ -1,9 +1,12 @@
 #import "ViewController.h"
 #import <TracksService.h>
 
-@interface ViewController ()
+@interface ViewController () <NSFetchedResultsControllerDelegate>
 
 @property (nonatomic, strong) TracksService *tracksService;
+@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+
+@property (nonatomic, weak) IBOutlet UILabel *objectCountLabel;
 
 @end
 
@@ -14,6 +17,16 @@
 
     self.tracksService = [[TracksService alloc] init];
     self.tracksService.queueSendInterval = 10.0;
+    
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"TracksEvent"];
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"date" ascending:YES];
+    fetchRequest.sortDescriptors = @[sortDescriptor];
+    self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+                                                                        managedObjectContext:self.tracksService.contextManager.managedObjectContext
+                                                                          sectionNameKeyPath:nil
+                                                                                   cacheName:nil];
+    self.fetchedResultsController.delegate = self;
+    [self.fetchedResultsController performFetch:nil];
 }
 
 
@@ -26,6 +39,14 @@
 - (IBAction)sendTestEvent:(id)sender
 {
     [self.tracksService trackEventName:@"test_event"];
+}
+
+
+#pragma mark - Fetched results delegate methods
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath;
+{
+    self.objectCountLabel.text = [NSString stringWithFormat:@"Number of events queued: %@", @(self.fetchedResultsController.fetchedObjects.count)];
 }
 
 @end
