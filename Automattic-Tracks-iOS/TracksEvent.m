705fa88f8c8d8c930ddb5d44cee05c44e7b16ce9
Aaron Douglas <astralbodies@gmail.com>
2015-04-06 13:57:53 -0500

    Changed some keys in TracksEvent to be named more like Objective-C

--- Automattic-Tracks-iOS/TracksEvent.m
+++ Automattic-Tracks-iOS/TracksEvent.m
@@ -1,13 +1,15 @@
 #import "TracksEvent.h"
 
-static NSString *const EVENT_NAME_KEY = @"_en";
+static NSString *const TracksEventNameKey = @"_en";
 static NSString *const USER_AGENT_NAME_KEY = @"_via_ua";
 static NSString *const TIMESTAMP_KEY = @"_ts";
-static NSString *const USER_TYPE_KEY = @"_ut";
-static NSString *const USER_TYPE_ANON = @"anon";
+static NSString *const TracksUserTypeKey = @"_ut";
+static NSString *const TracksUserIDKey = @"_ui";
+static NSString *const TracksUsernameKey = @"_ul";
+
+static NSString *const TracksUserTypeAnonymous = @"anon";
+static NSString *const TracksUserTypeWPCOM = @"wpcom:user_id";
 static NSString *const USER_ID_ANON = @"anonId";
-static NSString *const USER_ID_KEY = @"_ui";
-static NSString *const USER_LOGIN_NAME_KEY = @"_ul";
 
 @implementation TracksEvent
 
@@ -25,13 +27,19 @@ static NSString *const USER_LOGIN_NAME_KEY = @"_ul";
 - (NSDictionary *)dictionaryRepresentation
 {
     NSMutableDictionary *dict = [NSMutableDictionary new];
-    dict[EVENT_NAME_KEY] = self.eventName;
+    dict[TracksEventNameKey] = self.eventName;
 //    dict[USER_AGENT_NAME_KEY] = @"WPiOS";
     dict[TIMESTAMP_KEY] = @(lround(self.date.timeIntervalSince1970 * 1000));
 //    dict[USER_ID_ANON] = uuid;
     
     if (self.userType == TracksEventUserTypeAnonymous) {
-        dict[USER_TYPE_KEY] = USER_TYPE_ANON;
+        dict[TracksUserTypeKey] = TracksUserTypeAnonymous;
+        dict[TracksUserIDKey] = TracksUserTypeAnonymous;
+    } else {
+        dict[TracksUserTypeKey] = TracksUserTypeWPCOM;
+        dict[TracksUserIDKey] = @""; // TODO
+        dict[TracksUsernameKey] = self.user;
+        
     }
     
     [dict addEntriesFromDictionary:self.customProperties];
