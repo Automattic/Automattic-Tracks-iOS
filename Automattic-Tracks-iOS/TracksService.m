ef8bffa77911b9ac4ac27d0640801cfa1caa7b6b
Aaron Douglas <astralbodies@gmail.com>
2015-08-10 08:39:24 -0500

    Fixed the client properly

--- Automattic-Tracks-iOS/TracksService.m
+++ Automattic-Tracks-iOS/TracksService.m
@@ -234,8 +234,9 @@ NSString *const USER_ID_ANON = @"anonId";
     TracksDeviceInformation *deviceInformation = [TracksDeviceInformation new];
     
     CGSize screenSize = [[UIScreen mainScreen] bounds].size;
-    
-    return @{ RequestTimestampKey : [NSString stringWithFormat:@"%1.0f000", [NSDate date].timeIntervalSince1970],
+    long long since1970millis = [NSDate date].timeIntervalSince1970 * 1000;
+
+    return @{ RequestTimestampKey : @(since1970millis),
               DeviceInfoAppBuildKey : deviceInformation.appBuild ?: @"Unknown",
               DeviceInfoAppNameKey : deviceInformation.appName ?: @"Unknown",
               DeviceInfoAppVersionKey : deviceInformation.appVersion ?: @"Unknown",
@@ -264,9 +265,12 @@ NSString *const USER_ID_ANON = @"anonId";
 
 - (NSDictionary *)dictionaryForTracksEvent:(TracksEvent *)tracksEvent withParentCommonProperties:(NSDictionary *)parentCommonProperties
 {
+    NSTimeInterval since1970 = tracksEvent.date.timeIntervalSince1970;
+    long long since1970millis = since1970 * 1000;
+    
     NSMutableDictionary *dict = [NSMutableDictionary new];
     dict[TracksEventNameKey] = tracksEvent.eventName;
-    dict[TracksTimestampKey] = [NSString stringWithFormat:@"%1.0f000", tracksEvent.date.timeIntervalSince1970];
+    dict[TracksTimestampKey] = @(since1970millis);
     
     if (tracksEvent.userType == TracksEventUserTypeAnonymous) {
         dict[TracksUserTypeKey] = TracksUserTypeAnonymous;
