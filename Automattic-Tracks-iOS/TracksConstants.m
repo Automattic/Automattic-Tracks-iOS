a9ff69a1a85980a65778712168e8194cd37a1fcb
Aaron Douglas <astralbodies@gmail.com>
2015-08-13 14:27:58 -0500

    Version bump

--- Automattic-Tracks-iOS/TracksConstants.m
+++ Automattic-Tracks-iOS/TracksConstants.m
@@ -1,4 +1,4 @@
 #import "TracksConstants.h"
 
 NSString *const TracksErrorDomain = @"TracksErrorDomain";
-NSString *const TracksLibraryVersion = @"0.0.6";
+NSString *const TracksLibraryVersion = @"0.0.7";
