6de77030efdc18413666ff6eb8fd644b56b77c51
Jeremy Massel <jeremy.massel@gmail.com>
2018-11-21 11:14:53 -0700

    Update the Travis CI Configuration
    It now:
    - Uses a more modern xcode image
    - Updates both Cocoapods, and the spec repo _before_ trying to build (fixes issues with CP not being able to find the right versions of pods)
    - Moves the build script into it’s own shell script to make it nice to work with
    
    These changes are based mostly on the Aztec Editor’s TravisCI configuration, so using it should be low-risk – we already know it works great over there.

---
+++ Scripts/build.sh
@@ -0,0 +1,13 @@
+if [ ! $TRAVIS ]; then
+TRAVIS_XCODE_WORKSPACE=Automattic-Tracks-iOS.xcworkspace
+TRAVIS_XCODE_PROJECT=Automattic-Tracks-iOS.xcodeproj
+TRAVIS_XCODE_SCHEME=Automattic-Tracks-iOS
+TRAVIS_XCODE_SDK=iphonesimulator
+fi
+
+xcodebuild build test \
+-workspace "$TRAVIS_XCODE_WORKSPACE" \
+-scheme "$TRAVIS_XCODE_SCHEME" \
+-sdk "$TRAVIS_XCODE_SDK" \
+-destination "name=iPhone SE" \
+-configuration Debug | xcpretty -c && exit ${PIPESTATUS[0]}
