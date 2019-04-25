4ff5f166ccbbdb018cad51fcbd19c338303c3029
Diego Rey Mendez <diegoreymendez@gmail.com>
2019-04-25 15:23:31 -0300

    Updates the podspec version.

--- Automattic-Tracks-iOS/TracksConstants.m
+++ Automattic-Tracks-iOS/TracksConstants.m
@@ -1,4 +1,4 @@
 #import "TracksConstants.h"
 
 NSString *const TracksErrorDomain = @"TracksErrorDomain";
-NSString *const TracksLibraryVersion = @"0.3.4";
+NSString *const TracksLibraryVersion = @"0.3.5-beta.1";
