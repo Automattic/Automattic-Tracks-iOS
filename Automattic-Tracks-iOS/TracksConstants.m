ae2e25c5bf0c5a3bab9b5db4b631ba36c3bb60b7
Jeremy Massel <jeremy.massel@gmail.com>
2019-08-29 13:49:19 -0600

    Fix a merge issue

--- Automattic-Tracks-iOS/TracksConstants.m
+++ Automattic-Tracks-iOS/TracksConstants.m
@@ -1,8 +1,4 @@
 #import "TracksConstants.h"
 
 NSString *const TracksErrorDomain = @"TracksErrorDomain";
-<<<<<<< HEAD
-NSString *const TracksLibraryVersion = @"0.4.0";
-=======
 NSString *const TracksLibraryVersion = @"0.4.1";
->>>>>>> origin/develop
