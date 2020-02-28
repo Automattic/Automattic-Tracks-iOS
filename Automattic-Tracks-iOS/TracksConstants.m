4c4f3745aa5df949c5581fbdf309ef27ed22eb00
eToledo <etoledom@icloud.com>
2020-02-28 11:23:39 +0100

    Bump version to 0.4.4-beta.1

--- Automattic-Tracks-iOS/TracksConstants.m
+++ Automattic-Tracks-iOS/TracksConstants.m
@@ -1,4 +1,4 @@
 #import "TracksConstants.h"
 
 NSString *const TracksErrorDomain = @"TracksErrorDomain";
-NSString *const TracksLibraryVersion = @"0.4.3";
+NSString *const TracksLibraryVersion = @"0.4.4-beta.1";
