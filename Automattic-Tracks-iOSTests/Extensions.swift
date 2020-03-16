0e5ab337919bdce9c6a015e882bc4a173c4059b3
Jeremy Massel <jeremy.massel@automattic.com>
2020-03-16 15:55:25 -0600

    Move FileManager Extensions to test target

--- Automattic-Tracks-iOSTests/Extensions.swift
+++ Automattic-Tracks-iOSTests/Extensions.swift
@@ -17,3 +17,12 @@ extension Data {
         return [UInt8](self)
     }
 }
+
+extension FileManager {
+
+    func createTempFile(named name: String, containing contents: String?) -> URL {
+        let fileURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(name)
+        self.createFile(atPath: fileURL.path, contents: contents?.data(using: .utf8), attributes: nil)
+        return fileURL
+    }
+}
