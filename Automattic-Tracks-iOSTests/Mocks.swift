65a4fb4fdcaab48065a31fc7139d1a7e1a7643df
Jeremy Massel <jeremy.massel@automattic.com>
2020-03-16 16:12:34 -0600

    Remove callback types for now

--- Automattic-Tracks-iOSTests/Mocks.swift
+++ Automattic-Tracks-iOSTests/Mocks.swift
@@ -2,9 +2,6 @@ import Foundation
 import Sodium
 @testable import AutomatticTracks
 
-typealias LogFileCallback = (LogFile) -> ()
-typealias ErrorWithLogFileCallback = (Error, LogFile) -> ()
-
 enum MockError: Error {
     case generic
 }
