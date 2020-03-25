fac0f65f1637d9add3779a77b5ee3a22a654a3ab
Jeremy Massel <jeremy.massel@automattic.com>
2020-03-25 16:23:27 -0600

    Inject URLSession in EventLoggingNetworkService

--- Automattic-Tracks-iOS/Services/EventLoggingNetworkService.swift
+++ Automattic-Tracks-iOS/Services/EventLoggingNetworkService.swift
@@ -4,9 +4,15 @@ typealias NetworkResultCallback = (Result<Data?, Error>) -> Void
 
 open class EventLoggingNetworkService {
 
+
+    private let urlSession: URLSession
+
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+
     func uploadFile(request: URLRequest, fileURL: URL, completion: @escaping NetworkResultCallback) {
-        URLSession.shared
-            .uploadTask(with: request, fromFile: fileURL, completionHandler: { data, response, error in
+        urlSession.uploadTask(with: request, fromFile: fileURL, completionHandler: { data, response, error in
 
             if let error = error {
                 completion(.failure(error))
