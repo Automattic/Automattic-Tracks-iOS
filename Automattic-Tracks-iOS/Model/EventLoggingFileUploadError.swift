01408c788350b925d78c4f4cadbc7c5f5442846f
Jeremy Massel <jeremy.massel@automattic.com>
2020-03-25 16:32:16 -0600

    Fix code formatting

--- Automattic-Tracks-iOS/Model/EventLoggingFileUploadError.swift
+++ Automattic-Tracks-iOS/Model/EventLoggingFileUploadError.swift
@@ -4,7 +4,7 @@ public enum EventLoggingFileUploadError: Error, LocalizedError {
     case httpError(String)
     case fileMissing
 
-    public var errorDescription: String?{
+    public var errorDescription: String? {
         switch self {
             case .httpError(let message): return message
             case .fileMissing: return "File not found"
