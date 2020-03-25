ac7532802aef539c248490c9f10a142817194b70
Jeremy Massel <jeremy.massel@automattic.com>
2020-03-25 16:34:22 -0600

    Add localization for missing files error message
    We can’t guarantee it _won’t_ need to be translated.

--- Automattic-Tracks-iOS/Model/EventLoggingFileUploadError.swift
+++ Automattic-Tracks-iOS/Model/EventLoggingFileUploadError.swift
@@ -7,7 +7,9 @@ public enum EventLoggingFileUploadError: Error, LocalizedError {
     public var errorDescription: String? {
         switch self {
             case .httpError(let message): return message
-            case .fileMissing: return "File not found"
+            case .fileMissing: return NSLocalizedString(
+                "File not found", comment: "A message indicating that a file queued for upload could not be found"
+            )
         }
     }
 }
