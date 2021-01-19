43c95016773a8afc597e2e31c5ebfa71fefa5001
Leandro Alonso <contato@leandroalonso.com>
2021-01-19 12:24:43 -0300

    Bump version

--- Automattic-Tracks-iOS/TracksConstants.m
+++ Automattic-Tracks-iOS/TracksConstants.m
@@ -1,4 +1,4 @@
 #import "TracksConstants.h"
 
 NSString *const TracksErrorDomain = @"TracksErrorDomain";
-NSString *const TracksLibraryVersion = @"0.7.0";
+NSString *const TracksLibraryVersion = @"0.7.1";
