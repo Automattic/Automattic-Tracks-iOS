ae9db9faf812f7fa0943f6dd5fc5f75211cc67ac
BJ Homer <bjhomer@gmail.com>
2021-07-27 09:34:33 -0600

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,14 +1,4 @@
-XCODE_WORKSPACE=Automattic-Tracks-iOS.xcworkspace
-XCODE_PROJECT=Automattic-Tracks-iOS.xcodeproj
-XCODE_SCHEME=Automattic-Tracks-iOS
-XCODE_SDK=iphonesimulator
-
-xcodebuild build test \
--workspace "$XCODE_WORKSPACE" \
--scheme "$XCODE_SCHEME" \
--sdk "$XCODE_SDK" \
--destination "name=iPhone SE" \
--configuration Debug | bundle exec xcpretty -c
+swift build
 
 bundle exec pod lib lint
 
