d484d4a8146f765ae34e738119200788706beb35
Diego Rey Mendez <diego@reymendez.com>
2022-03-23 12:25:51 +0100

    Make some changes due to hound alerts

--- Sources/Event Logging (Swift)/TrackEventPersistenceService.swift
+++ Sources/Event Logging (Swift)/TrackEventPersistenceService.swift
@@ -9,68 +9,68 @@ import CoreData
 ///
 @objc
 public class TrackEventPersistenceService: NSObject {
-    
+
     private static let incrementRetryCountBatchSize = 500
     private let managedObjectContext: NSManagedObjectContext
-    
+
     @objc
     public init(managedObjectContext: NSManagedObjectContext) {
         self.managedObjectContext = managedObjectContext
-        
+
         super.init()
     }
-    
+
     /// Increments the retry count for the specified events in batches of
     @objc
     public func incrementRetryCountForEvents(_ tracksEvents: [TracksEvent], onComplete completion: ((Error?) -> Void)?) {
         let uuidStrings = tracksEvents.map { event in
             event.uuid.uuidString
         }
-        
+
         for startIndex in stride(from: 0, to: uuidStrings.count, by: Self.incrementRetryCountBatchSize) {
             let isLastBatch = startIndex + Self.incrementRetryCountBatchSize >= uuidStrings.count
-            
+
             managedObjectContext.perform {
                 let results: [TracksEventCoreData]
                 let count = min(uuidStrings.count - startIndex, Self.incrementRetryCountBatchSize)
                 let uuidStringsBatch = Array(uuidStrings[startIndex ..< startIndex + count])
-                
+
                 do {
                     results = try self.findTrackEventCoreData(uuidStrings: uuidStringsBatch)
                 } catch {
                     TracksLogError("Error while finding track events: \(String(describing: error))")
-                    
+
                     if isLastBatch {
                         completion?(error)
                     }
-                    
+
                     return
                 }
-                
+
                 if results.count != count {
                     TracksLogError("Not all provided events were found in the persistence layer. This signals a possible logical error in the tracking, persistence and retry-count-incrementing code.  Please review.")
                 }
-                
+
                 for event in results {
                     event.retryCount = event.retryCount.intValue + 1 as NSNumber
                 }
-                
+
                 self.saveManagedObjectContext()
-                
+
                 if isLastBatch {
                     completion?(nil)
                 }
             }
         }
     }
-    
+
     func findTrackEventCoreData(uuidStrings: [String]) throws -> [TracksEventCoreData] {
         let fetchRequest = NSFetchRequest<TracksEventCoreData>(entityName: "TracksEvent")
         fetchRequest.predicate = NSPredicate(format: "uuid in %@", uuidStrings)
-        
+
         return try fetchRequest.execute()
     }
-    
+
     func saveManagedObjectContext() {
         do {
             try managedObjectContext.save()
