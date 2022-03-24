019fbaf2b0c2f0a8f3a1f944a430fd9e8d08cf4c
Diego Rey Mendez <diego@reymendez.com>
2022-03-24 11:09:49 +0100

    swiftline --fix

--- Sources/Event Logging (Swift)/TracksEventPersistenceServiceSwift.swift
+++ Sources/Event Logging (Swift)/TracksEventPersistenceServiceSwift.swift
@@ -50,7 +50,7 @@ public class TracksEventPersistenceServiceSwift: NSObject {
 
                 self.saveManagedObjectContext()
             }
-            
+
             completion?()
         }
     }
