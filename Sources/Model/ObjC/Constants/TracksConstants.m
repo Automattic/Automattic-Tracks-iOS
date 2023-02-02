3c15eb0450aa5f70aff29bc2e465e6005ce41a47
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-02-02 08:49:55 +0530

    Update library version in constants to fix CI.

--- Sources/Model/ObjC/Constants/TracksConstants.m
+++ Sources/Model/ObjC/Constants/TracksConstants.m
@@ -1,4 +1,4 @@
 #import "TracksConstants.h"
 
 NSString *const TracksErrorDomain = @"TracksErrorDomain";
-NSString *const TracksLibraryVersion = @"1.0.0";
+NSString *const TracksLibraryVersion = @"1.1.0-beta";
