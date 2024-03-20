bd891fce2c5943f7c390b287f0d618abbe48e5f8
Tony Li <tony.li@automattic.com>
2024-03-21 12:15:06 +1300

    Update version in the library code

--- Sources/Model/ObjC/Constants/TracksConstants.m
+++ Sources/Model/ObjC/Constants/TracksConstants.m
@@ -1,4 +1,4 @@
 #import "TracksConstants.h"
 
 NSString *const TracksErrorDomain = @"TracksErrorDomain";
-NSString *const TracksLibraryVersion = @"3.3.0";
+NSString *const TracksLibraryVersion = @"3.4.0";
